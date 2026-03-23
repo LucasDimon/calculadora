a99cc5c0704c985417e3962f4fa1a0dfd4c4a9a5
LucasDimon <lucas_dimon@estudante.sesisenai.org.br>
2026-03-23 20:52:57 -0300

    feat: adiciona operacao de potencia

--- calculadora.js
+++ calculadora.js
@@ -10,12 +10,15 @@ function calculadora(num1, num2, operacao) {
   }
 
   else if (operacao === "*") {
-    // ERRO 2: multiplicação soma +2 no resultado : corrigido
+    
     return (num1 * num2);
   }
 
   else if (operacao === "/") {
     return num1 / num2;
+  }
+  else if (operacao === "**") {
+    return num1 ** num2; // Funcionalidade 2: Potência
   }
 
   else {
@@ -29,3 +32,4 @@ console.log("2 + 2 =", calculadora(2, 2, "+"));
 console.log("3 * 3 =", calculadora(3, 3, "*")); 
 console.log("10 - 5 =", calculadora(10, 5, "-"));
 console.log("8 / 2 =", calculadora(8, 2, "/"));
+console.log("2 ** 3 =", calculadora(2, 3, "**"));
