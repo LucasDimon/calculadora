8615aacbc42c6ab54c432733aa89b1d8b1da0f6f
ADRISON SIDNE SANTOS DE MORAIS <adrison_morais@estudante.sesisenai.org.br>
2026-03-23 20:47:09 -0300

    feat:adiciona operação de resto da divisão

--- calculadora.js
+++ calculadora.js
@@ -17,6 +17,10 @@ function calculadora(num1, num2, operacao) {
   else if (operacao === "/") {
     return num1 / num2;
   }
+  else if(operacao === "%"){
+    return num1 % num2; // Funcionalidade 1 resto da Divisão
+
+  }
 
   else {
     return "Operação inválida";
@@ -29,3 +33,4 @@ console.log("2 + 2 =", calculadora(2, 2, "+"));
 console.log("3 * 3 =", calculadora(3, 3, "*")); 
 console.log("10 - 5 =", calculadora(10, 5, "-"));
 console.log("8 / 2 =", calculadora(8, 2, "/"));
+console.log("10 % 3 =", calculadora(10, 3, "%"));
